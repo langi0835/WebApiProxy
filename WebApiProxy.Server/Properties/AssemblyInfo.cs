9e23cae3860cdbd8df6f82c32845a84ac2a08632
Lanpipi <aaa@mail.com>
2020-05-27 22:36:02 +0800

    固定元件版號

--- WebApiProxy.Server/Properties/AssemblyInfo.cs
+++ WebApiProxy.Server/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("1.3.*")]
-[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("1.3.0")]
